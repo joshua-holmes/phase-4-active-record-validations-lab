3bf5222bfe359179aff43327c1b1588a381b341f
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-11 12:25:31 -0600

    commit tree 75ad7a3abd9f0e4f1209b0b154aa8f5797f2b1e9

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
     validates :title, exclusion: { in: ["True Facts"] }
+
+    validate :click_bate
+
+    def click_bate
+        unless title.match?(/Won't Believe|Secret|Top [0-9]|Guess/)
+            errors.add(:title, "Must contain clickbatey element")
+        end if title
+    end
 end
